880bd6d029e24ab05cd0bf8caf166f6dc1dbee8c
Kpurek <ktsq2000@126.com>
2020-06-14 19:25:22 +0800

    commit client view

---
+++ view/clientView.java
@@ -0,0 +1,14 @@
+package view;
+
+public class clientView {
+
+    public static void main(String[] args) {
+        new clientView();
+    }
+    public clientView(){
+        initView();
+    }
+
+    private void initView() {
+    }
+}
