9a0491e0c89df6fd9418d95339ed4431d0db6d96
Kpure1000 <ktsq200@126.com>
2020-06-14 15:22:32 +0800

    commit code

---
+++ service/ClientNetwork.java
@@ -0,0 +1,59 @@
+package service;
+
+public class ClientNetwork {
+
+    public ClientNetwork(CallBack callBack) {
+        this.callBack = callBack;
+    }
+
+    /**
+     * 连接服务器
+     * 
+     * @param host 主机号
+     * @param port 端口
+     */
+    public void connect(String host, int port) {
+
+    }
+
+    /**
+     * 断开连接
+     */
+    public void disconnect() {
+
+    }
+
+    /**
+     * 发消息
+     * 
+     * @param name 用户名
+     * @param msg  消息
+     */
+    public void sendMessage(String name, String msg) {
+
+    }
+
+    /**
+     * 已/未连接
+     * 
+     * @return 是否连接
+     */
+    public boolean isConnected() {
+
+    }
+
+    public interface CallBack {
+
+        void onConnected(String host, int port);
+
+        void onConnectFailed(String host, int port);
+
+        void onDisconnected();
+
+        void onMessageSent();
+
+        void onMessageReceived(String name, String msg);
+    }
+
+    private CallBack callBack;
+}
---
+++ service/ServiceSocket.java
@@ -0,0 +1,10 @@
+package service;
+/**
+ * ServiceSocket
+ */
+public class ServiceSocket {
+
+    private ServiceSocket serviceSocket;
+
+    
+}
