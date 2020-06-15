6d20e62c4d4601d7cdd34b2ba6cdd07fd7088ad1
ktsq <ktsq2000@PianodeMacBook-Air-2.local>
2020-06-15 18:56:58 +0800

    move userinfo from net to view

--- service/ClientNetwork.java
+++ service/ClientNetwork.java
@@ -1,11 +1,32 @@
 package service;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.security.auth.callback.Callback;
+
+import infomation.userInfo;
+
+/**
+ * 客户端网络类
+ * 
+ * @author Kpurek
+ * @version 1.0
+ */
 public class ClientNetwork {
 
-    private boolean connected;
-    public ClientNetwork(CallBack callBack) {
-        this.callBack = callBack;
-    }
+    /*--------------------------------------------*/
+
+    // 套接字
+    private Socket socket;
+    // 输入
+    private DataInputStream inputStream;
+    // 输出
+    private DataOutputStream outputStream;
+    // 连接标志
+    private boolean isConnected;
 
     /**
      * 连接服务器
@@ -14,14 +35,64 @@ public class ClientNetwork {
      * @param port 端口
      */
     public void connect(String host, int port) {
+        try {
+            socket = new Socket(host, port);
+            isConnected = true;
+            if (callBack != null) {
+                callBack.onConnectSuccess(host, port);
+            }
+            beginListening();
+        } catch (IOException e) {
+            isConnected = false;
+            if (callBack != null) {
+                callBack.onConnectFailed(host, port);
+            }
+            e.printStackTrace();
+        }
+    }
 
+    private void beginListening() {
+        Runnable listenRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    inputStream = new DataInputStream(socket.getInputStream());
+                    while (true) {
+                        var inputStr = inputStream.readUTF().split("#");
+                        if (callBack != null) {
+                            callBack.onMessageReceived(inputStr[0], inputStr[1]);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        (new Thread(listenRunnable)).start();
     }
 
     /**
      * 断开连接
      */
     public void disconnect() {
-
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            isConnected = false;
+            if (callBack != null) {
+                callBack.onDisconnected();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -30,8 +101,23 @@ public class ClientNetwork {
      * @param name 用户名
      * @param msg  消息
      */
-    public void sendMessage(String name, String msg) {
-
+    public void sendMessage(String id, String msg) {
+        try {
+            if (msg == null || "".equals(msg) || socket == null) {
+                return;
+            }
+            if (outputStream != null) {
+                String outputStr = id + msg;
+                outputStream = new DataOutputStream(socket.getOutputStream());
+                outputStream.writeUTF(outputStr);
+                outputStream.flush();
+                if (callBack != null) {
+                    callBack.onMessageSent(id, msg);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -40,20 +126,42 @@ public class ClientNetwork {
      * @return 是否连接,true为连接，false为未连接
      */
     public boolean isConnected() {
-        return this.connected;
+        return this.isConnected;
     }
 
     public interface CallBack {
 
-        void onConnected(String host, int port);
+        /**
+         * 当连接成功时
+         * 
+         * @param host 主机号
+         * @param port 端口号
+         */
+        void onConnectSuccess(String host, int port);
 
+        /**
+         * 当连接失败时
+         * 
+         * @param host 主机号
+         * @param port 端口号
+         */
         void onConnectFailed(String host, int port);
 
+        /**
+         * 当断开连接时
+         */
         void onDisconnected();
 
-        void onMessageSent();
+        /**
+         * 当发送消息时
+         */
+        void onMessageSent(String id, String msg);
 
-        void onMessageReceived(String name, String msg);
+        /**
+         * @param id  消息源ID
+         * @param msg 消息内容
+         */
+        void onMessageReceived(String id, String msg);
     }
 
     private CallBack callBack;
--- view/clientView.java
+++ view/clientView.java
@@ -1,14 +1,47 @@
 package view;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.security.auth.callback.Callback;
+
+import service.ClientNetwork;
+
+/**
+ * 客户端视图
+ * 
+ * @author Kpurek
+ * @version 1.0
+ */
 public class clientView {
+
+    Scanner sc;
+
+    ClientNetwork clientNetwork;
 
     public static void main(String[] args) {
         new clientView();
     }
-    public clientView(){
-        initView();
+
+    /**
+     * 构造
+     */
+    public clientView() {
+        try {
+            clientNetwork = new ClientNetwork();
+            initLoginView();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void initView() {
+    /**
+     * 初始化登录界面
+     */
+    private void initLoginView() throws IOException {
+        System.out.print("***登录**:\n");
+        System.out.print("*账号: ");
+        String IDstr = sc.nextLine();
+
     }
 }
